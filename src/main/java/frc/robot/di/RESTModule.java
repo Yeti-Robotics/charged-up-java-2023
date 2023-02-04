f2677aba1321da1abebd20f9a66e9173a7dc7c9c
vwu1888 <vwu1888@gmail.com>
2023-02-04 10:48:31 -0500

    Removed deprecated Button class from ButtonHelper

--- src/main/java/frc/robot/di/RESTModule.java
+++ src/main/java/frc/robot/di/RESTModule.java
@@ -2,6 +2,8 @@ package frc.robot.di;
 
 import dagger.Module;
 import dagger.Provides;
+import dagger.multibindings.IntoSet;
+import frc.robot.commands.rest.ExampleREST;
 import frc.robot.utils.rests.restUtils.RESTContainer;
 import frc.robot.utils.rests.restUtils.RESTHandler;
 
@@ -16,5 +18,10 @@ public class RESTModule {
     public RESTHandler providesRESTHandler(Set<RESTContainer> rests) {
         ArrayList<RESTContainer> restList = new ArrayList<>(rests);
         return new RESTHandler(restList);
+    }
+
+    @Provides @IntoSet
+    public RESTContainer providesExampleREST() {
+        return new ExampleREST();
     }
 }
--- src/main/java/frc/robot/di/RobotModule.java
+++ src/main/java/frc/robot/di/RobotModule.java
@@ -7,18 +7,27 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.utils.controllerUtils.ButtonHelper;
 import frc.robot.utils.controllerUtils.ControllerContainer;
 
 import javax.inject.Singleton;
+import java.awt.*;
 import java.util.Map;
 
 @Module
 public class RobotModule {
     @Provides
     @Singleton
-    public RobotContainer providesRobotContainer(ExampleSubsystem exampleSubsystem,ControllerContainer controllerContainer, Map<Class<?>, CommandBase> commands) {
+    public RobotContainer providesRobotContainer(
+            ExampleSubsystem exampleSubsystem,
+            ControllerContainer controllerContainer,
+            ButtonHelper buttonHelper,
+            Map<Class<?>, CommandBase> commands) {
         return new RobotContainer(
-                exampleSubsystem, controllerContainer, commands
+                exampleSubsystem,
+                controllerContainer,
+                buttonHelper,
+                commands
         );
     }
 
@@ -28,4 +37,9 @@ public class RobotModule {
         return new ControllerContainer();
     }
 
+    @Provides
+    @Singleton
+    public ButtonHelper providesButtonHelper(ControllerContainer controllerContainer) {
+        return new ButtonHelper(controllerContainer.getControllers());
+    }
 }
