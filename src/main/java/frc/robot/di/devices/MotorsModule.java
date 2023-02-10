670fcfca7c9071d849f25afc9baa9fb355edd03a
sanskardeva <sanskarkulshreshtha@gmail.com>
2023-02-09 21:06:56 -0500

    added current limiting, seperated to talonfx constants

--- src/main/java/frc/robot/di/devices/MotorsModule.java
+++ src/main/java/frc/robot/di/devices/MotorsModule.java
@@ -1,6 +1,7 @@
 package frc.robot.di.devices;
 
 import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import dagger.Module;
 import dagger.Provides;
@@ -12,11 +13,15 @@ import javax.inject.Singleton;
 
 @Module
 public class MotorsModule {
+    @Provides
+    public SupplyCurrentLimitConfiguration elevatorCurrentConfig(){
+        return new SupplyCurrentLimitConfiguration(true, 0.0, 0.0, 0.0);
+    }
 
     @Provides
     @Singleton
     @Named("elevatorMotor")
-    public WPI_TalonFX elevatorMotor() {
+    public WPI_TalonFX elevatorMotor(SupplyCurrentLimitConfiguration elevatorCurrentConfig) {
         WPI_TalonFX motor = new WPI_TalonFX(Constants.ElevatorConstants.ELEVATOR_MOTOR);
 
         motor.config_kP(0, Constants.ElevatorConstants.ELEVATOR_P);
@@ -28,7 +33,7 @@ public class MotorsModule {
         motor.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10);
         motor.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10);
         motor.configPeakOutputForward(.6);
-
+        motor.configSupplyCurrentLimit(elevatorCurrentConfig);
         motor.configClosedloopRamp(0.3);
 
 
