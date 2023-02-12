5e53ee9f766200d46ae451211881c6c1267d6860
EPatel6 <eeshapatel67@gmail.com>
2023-02-11 21:14:34 -0500

    constants are correct :milkshake:

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,8 +23,8 @@ public final class Constants
     }
 
     public static final class ArmConstants{
-        public static final int MOTOR_1 = 9; // We don't know yet
-        public static final int MOTOR_2 = 10;
+        public static final int MOTOR_1 = 10; // left arm
+        public static final int MOTOR_2 = 11; //right arm
         public static final double ARM_SPEED = 0.5;
 
         public static final double VOLTAGE_COMP = 12.0;
