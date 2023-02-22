76371fd80d00f6133d6fe4b949894c3a9453aaa5
vwu0000 <vwu00000@email.cpcc.edu>
2023-02-22 17:19:38 -0500

    handoff commands

---
+++ src/main/java/frc/robot/commands/CubeHandoffCommand.java
@@ -0,0 +1,30 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import frc.robot.Constants;
+import frc.robot.commands.arm.SetArmPositionCommand;
+import frc.robot.commands.carriage.ConeInCubeOutCommand;
+import frc.robot.commands.elevator.SetElevatorPositionCommand;
+import frc.robot.commands.intake.IntakeRollOutCommand;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.CarriageSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class CubeHandoffCommand extends SequentialCommandGroup {
+    public CubeHandoffCommand(
+            ArmSubsystem armSubsystem,
+            IntakeSubsystem intakeSubsystem,
+            ElevatorSubsystem elevatorSubsystem,
+            CarriageSubsystem carriageSubsystem) {
+        addCommands(
+                new SetElevatorPositionCommand(elevatorSubsystem, Constants.ElevatorConstants.ElevatorPositions.CONE_HANDOFF),
+                new SetArmPositionCommand(armSubsystem, Constants.ArmConstants.ArmPositions.HANDOFF),
+                new InstantCommand(intakeSubsystem::intakeOpen, intakeSubsystem),
+                new IntakeRollOutCommand(intakeSubsystem).withTimeout(1.0)
+                        .alongWith(new ConeInCubeOutCommand(carriageSubsystem)).withTimeout(2.5)
+        );
+    }
+}
