06c6142e96a83559a03f66fa61abb512cef1a9b1
VGopal06 <58563042+VGopal06@users.noreply.github.com>
2023-03-23 15:47:53 -0700

    Swtiched intake to use one talon

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -1,10 +1,9 @@
 package frc.robot.constants;
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.util.Units;
 
 public final class IntakeConstants {
-    public static final String LEFT_SPARK = "intake spark 1";
+    public static final String TALON = "intake spark 1";
     public static final int LEFT_SPARK_ID = 1;
     public static final String RIGHT_SPARK = "intake spark 2";
     public static final int RIGHT_SPARK_ID = 2;
