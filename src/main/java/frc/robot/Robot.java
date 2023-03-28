b802c8421b3f8cc10f403d3e89899086b4c25bf1
Aarush Mehta <eighthrush@gmail.com>
2023-03-28 17:31:07 -0500

    Autos for Bump

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -72,6 +72,7 @@ public class Robot extends TimedRobot {
         autoChooser.addOption(AutoModes.CONE_BALANCE_TWO.name, AutoModes.CONE_BALANCE_TWO);
         autoChooser.addOption(AutoModes.CONE_BALANCE_THREE.name, AutoModes.CONE_BALANCE_THREE);
         autoChooser.addOption(AutoModes.TWO_PIECE_BALANCE_ONE.name, AutoModes.TWO_PIECE_BALANCE_ONE);
+        autoChooser.addOption(AutoModes.MID_TWO_PIECE_BALANCE_ONE.name, AutoModes.MID_TWO_PIECE_BALANCE_ONE);
         autoChooser.addOption(AutoModes.MID_TWO_PIECE_BALANCE_TWO.name, AutoModes.MID_TWO_PIECE_BALANCE_TWO);
         autoChooser.addOption(AutoModes.MID_TWO_PICKUP_BALANCE_TWO.name, AutoModes.MID_TWO_PICKUP_BALANCE_TWO);
         autoChooser.addOption(AutoModes.CONE_ONE.name, AutoModes.CONE_ONE);
